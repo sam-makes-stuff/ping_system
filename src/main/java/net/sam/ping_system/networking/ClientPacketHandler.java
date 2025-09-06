1969a0bf5cc301dcd3d729b3a899aec835ef73f3
samuelpuclin <samuel.puclin@gmail.com>
2025-09-06 20:14:56 +1000

    Pinging now sends chat message
    Pinging now sends a chat message to all players on the same team, with the senders name, the block they pinged, the type of ping, and in the team's colour.

--- src/main/java/net/sam/ping_system/networking/ClientPacketHandler.java
+++ src/main/java/net/sam/ping_system/networking/ClientPacketHandler.java
@@ -2,16 +2,54 @@
 package net.sam.ping_system.networking;
 
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.GuiMessage;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.scores.Team;
+import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.sam.ping_system.client.overlay.PingHandler;
 
 public class ClientPacketHandler {
-
     public static void handleS2CPingPacket(int senderId, int type, double x, double y, double z, BlockPos blockPos, int r, int g, int b) {
         Minecraft mc = Minecraft.getInstance();
+        LocalPlayer player = mc.player;
+        Level level = mc.player.level();
+        String blockName = level.getBlockState(blockPos).getBlock().getName().getString();
+        Entity sender = mc.player.level().getEntity(senderId);
+        Team team = sender.getTeam();
+
+        ChatFormatting teamColor;
+        if(team != null){
+            teamColor = sender.getTeam().getColor();
+        }else {
+            teamColor = ChatFormatting.WHITE;
+        }
+
+        if(sender instanceof Player p){
+            String message = String.format("%s - %s", p.getName().getString(), blockName);
+            if(type == 1){
+                message += " (GO)";
+            } else if (type == 2) {
+                message += " (ATTACK)";
+            }else if (type == 3) {
+                message += " (DANGER)";
+            }else if (type == 4) {
+                message += " (BREAK)";
+            }
+            player.sendSystemMessage(Component.literal(message).withStyle(teamColor));
+        }
+
         PingHandler.newPing(senderId, type, x,y,z,r,g,b);
     }
 }
